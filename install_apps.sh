f262e19b626d55e6ed6e8bf1f17929ac2ad12df1
Heitor Santos <heitor.santos@gmail.com>
2025-09-14 19:50:37 -0300

    Script melhorado.

--- install_apps.sh
+++ install_apps.sh
@@ -1,94 +1,53 @@
 #!/usr/bin/env bash
 
-echo " "
+set -e # Para o script se houver erro
+
 echo "---------------------"
 echo "Atualizando sistema..."
 
+# Cria usuário com senha pré-definida
 useradd -m -G wheel heitorpbds
-passwd heitorpbds
+echo "heitorpbds:senha123" | chpasswd
 
-pacman -Suy
+pacman -Suy --noconfirm
 
 hostnamectl hostname arch-acer
 timedatectl set-timezone America/Sao_Paulo
-
-timedatectl set-ntp TRUE
+timedatectl set-ntp true
 echo LANG="en_US.UTF-8" >> /etc/locale.conf
 localectl set-keymap us
 
-pacman -S reflector sudo vim
+pacman -S --noconfirm reflector sudo vim
 reflector -c Brazil -a 6 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
-pacman -Suy
+pacman -Suy --noconfirm
 
-
-echo " "
 echo "---------------------"
 echo "Instalando os apps..."
-pacman -S --needed \
-  xdg-user-dirs \
-  fastfetch \
-  git \
-  base-devel \
-  curl \
-  wget \
-  nano \
-  vim \
-  networkmanager \
-  zsh \
-  zip \
-  unzip \
-  ffmpeg \
-  ntfs-3g \
-  docker \
-  docker-compose \
-  kdenlive \
-  audacity \
-  hplip \
-  print-manager \
-  system-config-printer \
-  ffmpegthumbs \
-  steam \
-  bitwarden \
-  dosfstools \
-  gnome-builder \
-  linux-headers \ 
-  gnome-shell \
-  gnome-terminal \
-  gnome-control-center \
-  gnome-tweaks \
-  gnome-backgrounds \
-  nautilus \
-  gdm \
-  nvidia \
-  nvidia-prime \
-  xorg-xrandr \
-  libreoffice-fresh 
+pacman -S --needed --noconfirm \
+  xdg-user-dirs fastfetch git base-devel curl wget nano vim networkmanager \
+  zsh zip unzip ffmpeg ntfs-3g docker docker-compose kdenlive audacity hplip \
+  print-manager system-config-printer ffmpegthumbs steam bitwarden dosfstools \
+  gnome-builder linux-headers gnome-shell gnome-terminal gnome-control-center \
+  gnome-tweaks gnome-backgrounds nautilus gdm nvidia nvidia-prime xorg-xrandr \
+  libreoffice-fresh
 
-  
-  
-echo " "
 echo "-----------------"
 echo "Instalando Paru..."
-cd ~ && git clone https://aur.archlinux.org/paru.git && cd paru && makepkg -si
+cd /tmp
+git clone https://aur.archlinux.org/paru.git || exit 1
+cd paru
+makepkg -si --noconfirm || exit 1
 
-echo " "
 echo "----------------------"
 echo "Instalando apps paru..."
 paru -S --needed --noconfirm \
-    google-chrome \
-    webapp-manager \
-    youtube-music-bin \
-    visual-studio-code-bin \
-    extension-manager \
-    zoom
-    
-echo " "
-echo "----------------------------------------"
-echo "Instalando oh-my-zsh..." 
-cd ~
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    google-chrome webapp-manager youtube-music-bin visual-studio-code-bin \
+    extension-manager zoom
 
+echo "----------------------------------------"
+echo "Instalando oh-my-zsh para heitorpbds..."
+runuser -u heitorpbds -- sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+chsh -s /bin/zsh heitorpbds
 
 echo "Iniciar o Gnome"
-su heitorpbds
-sudo  systemctl enable --now gdm
+systemctl enable --now gdm
